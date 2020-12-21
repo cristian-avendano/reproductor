c5ae24313452713cdaf94b68fe87b2fa499eba78
cristian-avendano <cristian-avendano@upc.edu.co>
2020-12-21 14:12:31 -0500

    add lateral color

--- public/js/index.js
+++ public/js/index.js
@@ -10,16 +10,28 @@ setOp.addEventListener('click', ()=>{
     setCont.style.display = "block";
     libCont.style.display = "none";
     playCont.style.display = "none";
+
+    setOp.style.borderLeft="solid 2px deeppink";
+    libOp.style.borderLeft="0px";
+    playOp.style.borderLeft="0px";
 });
 
 libOp.addEventListener('click', ()=>{
     setCont.style.display = "none";
     libCont.style.display = "flex";
     playCont.style.display = "none";
+
+    setOp.style.borderLeft="0px";
+    libOp.style.borderLeft="solid 2px deeppink";
+    playOp.style.borderLeft="0px";
 });
 
 playOp.addEventListener('click', ()=>{
     setCont.style.display = "none";
     libCont.style.display = "none";
     playCont.style.display = "block";
+
+    setOp.style.borderLeft="0px";
+    libOp.style.borderLeft="0px";
+    playOp.style.borderLeft="solid 2px deeppink";
 });
